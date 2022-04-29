272289a910657cea312a1dac10677ae949e9fa4a
SSK137 <saurabhkodam@gmail.com>
2022-04-29 22:59:04 +0530

    AddressBooksystem : Contacts In Address Book

--- src/AddressBooksystem.java
+++ src/AddressBooksystem.java
@@ -1,5 +1,15 @@
 public class AddressBooksystem {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Systems");
+
+        Contacts contacts = new Contacts("Saurabh", "Kodam", "Savedi ", "Ahmednagar", "Maharashtra","414003", "8149355350","saurabhkodam@gmail.com");
+        System.out.println(contacts.getFirstName());
+        System.out.println(contacts.getLastName());
+        System.out.println(contacts.getEmail());
+        System.out.println(contacts.getAddress());
+        System.out.println(contacts.getCity());
+        System.out.println(contacts.getState());
+        System.out.println(contacts.getContactNo());
+        System.out.println(contacts.getZipCode());
     }
 }
